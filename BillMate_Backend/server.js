fdeb0d4bae88b52a230a0db4abd14950ddab6bd1
dinakaranak <dinakaranak001@gmail.com>
2025-08-31 11:43:05 +0530

    push

--- BillMate_Backend/server.js
+++ BillMate_Backend/server.js
@@ -71,8 +71,8 @@ if (!process.env.MONGO_URI) {
 }
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
 .then(() => console.log("✅ MongoDB connected"))
 .catch(err => console.error("❌ MongoDB connection error:", err));
